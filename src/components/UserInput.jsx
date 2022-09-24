46ace4bde8a4d85bed59bb417969d5e3f1e1bd3d
johnazt <john.azt.dev@gmail.com>
2022-09-24 15:05:44 -0300

    Fix bg-bottom

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -34,9 +34,9 @@ const UserInput = () => {
 					/>
 					<button onClick={getUser}>Comenzar</button>
 				</div>
-			</div>
 			<div className="bg-red-black">
 				<div className="pokeball"></div>
+			</div>
 			</div>
 		</div>
 	);
