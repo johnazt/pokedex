871ee01eba7b57131ff75387694834d99844ec91
johnazt <john.azt.dev@gmail.com>
2022-09-25 18:27:02 -0300

    Add colors pokemoncards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -16,11 +16,97 @@ const PokemonCard = props => {
 		axios.get(props.url).then(res => setPokemon(res.data));
 	}, []);
 
+	// ========= RENDER COLORS POKEMON ============== //
+
 	useEffect(() => {
-		if (types?.[0].type?.name === "fire") {
-			setLinearColor("var(--fire)");
-			setBgColor("#DE701C");
-			setTextColor("#E75C35");
+		switch (types?.[0].type?.name) {
+			case "fire":
+				setLinearColor("var(--linear-fire)");
+				setBgColor("var(--bg-fire)");
+				setTextColor("var(--text-fire)");
+				break;
+			case "grass":
+				setLinearColor("var(--linear-grass)");
+				setBgColor("var(--bg-grass)");
+				setTextColor("var(--text-grass)");
+				break;
+			case "water":
+				setLinearColor("var(--linear-water)");
+				setBgColor("var(--bg-water)");
+				setTextColor("var(--text-water)");
+				break;
+			case "bug":
+				setLinearColor("var(--linear-bug)");
+				setBgColor("var(--bg-bug)");
+				setTextColor("var(--text-bug)");
+				break;
+			case "normal":
+				setLinearColor("var(--linear-normal)");
+				setBgColor("var(--bg-normal)");
+				setTextColor("var(--text-normal)");
+				break;
+			case "fighting":
+				setLinearColor("var(--linear-fighting)");
+				setBgColor("var(--bg-fighting)");
+				setTextColor("var(--text-fighting)");
+				break;
+			case "poison":
+				setLinearColor("var(--linear-poison)");
+				setBgColor("var(--bg-poison)");
+				setTextColor("var(--text-poison)");
+				break;
+			case "ghost":
+				setLinearColor("var(--linear-ghost)");
+				setBgColor("var(--bg-ghost)");
+				setTextColor("var(--text-ghost)");
+				break;
+			case "rock":
+				setLinearColor("var(--linear-rock)");
+				setBgColor("var(--bg-rock)");
+				setTextColor("var(--text-rock)");
+				break;
+			case "dark":
+				setLinearColor("var(--linear-dark)");
+				setBgColor("var(--bg-dark)");
+				setTextColor("var(--text-dark)");
+				break;
+			case "ice":
+				setLinearColor("var(--linear-ice)");
+				setBgColor("var(--bg-ice)");
+				setTextColor("var(--text-ice)");
+				break;
+			case "steel":
+				setLinearColor("var(--linear-steel)");
+				setBgColor("var(--bg-steel)");
+				setTextColor("var(--text-steel)");
+				break;
+			case "electric":
+				setLinearColor("var(--linear-electric)");
+				setBgColor("var(--bg-electric)");
+				setTextColor("var(--text-electric)");
+				break;
+			case "fairy":
+				setLinearColor("var(--linear-fairy)");
+				setBgColor("var(--bg-fairy)");
+				setTextColor("var(--text-fairy)");
+				break;
+			case "psychic":
+				setLinearColor("var(--linear-psychic)");
+				setBgColor("var(--bg-psychic)");
+				setTextColor("var(--text-psychic)");
+				break;
+			case "ground":
+				setLinearColor("var(--linear-ground)");
+				setBgColor("var(--bg-ground)");
+				setTextColor("var(--text-ground)");
+				break;
+			case "dragon":
+				setLinearColor("var(--linear-dragon)");
+				setBgColor("var(--bg-dragon)");
+				setTextColor("var(--text-dragon)");
+				break;
+			default:
+				break;
 		}
 	}, [pokemon]);
 
@@ -43,7 +129,9 @@ const PokemonCard = props => {
 			</div>
 
 			<div className="pokemon-card-info">
-				<p className="pokemon-card-name">{pokemon.name}</p>
+				<p style={{ color: textColor }} className="pokemon-card-name">
+					{pokemon.name}
+				</p>
 				<div>
 					{types?.map(elem => (
 						<span className="pokemon-card-type" key={elem.slot}>
