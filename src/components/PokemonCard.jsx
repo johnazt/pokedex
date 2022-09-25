fc9a80ae820c360a723cee80a44f3997c3d86721
johnazt <john.azt.dev@gmail.com>
2022-09-25 16:03:38 -0300

    Add color

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -8,18 +8,29 @@ const PokemonCard = props => {
 	const navigate = useNavigate();
 	const [pokemon, setPokemon] = useState({});
 	const types = pokemon.types;
+	const [linearColor, setLinearColor] = useState("");
+	const [bgColor, setBgColor] = useState("");
+	const [textColor, setTextColor] = useState("");
 
 	useEffect(() => {
 		axios.get(props.url).then(res => setPokemon(res.data));
 	}, []);
 
+	useEffect(() => {
+		if (types?.[0].type?.name === "fire") {
+			setLinearColor("var(--fire)");
+			setBgColor("#DE701C");
+			setTextColor("#E75C35");
+		}
+	}, [pokemon]);
 
 	return (
 		<div
 			className="pokemon-card"
+			style={{ backgroundColor: bgColor }}
 			onClick={() => navigate(`/pokemons/${pokemon.id}`)}
 		>
-			<div className="pokemon-card-img">
+			<div className="pokemon-card-img" style={{ background: linearColor }}>
 				<img
 					src={
 						pokemon.sprites?.other?.["official-artwork"].front_default !== null
@@ -33,32 +44,40 @@ const PokemonCard = props => {
 
 			<div className="pokemon-card-info">
 				<p className="pokemon-card-name">{pokemon.name}</p>
-				<div >
+				<div>
 					{types?.map(elem => (
 						<span className="pokemon-card-type" key={elem.slot}>
-							{(elem.type.name).charAt(0).toUpperCase() + (elem.type.name).slice(1)}
+							{elem.type.name.charAt(0).toUpperCase() + elem.type.name.slice(1)}
 							{types.length > 1 && types.indexOf(elem) === 0 ? " / " : ""}
 						</span>
 					))}
 				</div>
-				<span style={{ color: "#9F9F9F", paddingBottom:"1em" }}>Type</span>
-				<hr style={{ width: "100%", border:"none", borderTop:"1px solid #E5E5E5"} } />
+				<span style={{ color: "#9F9F9F", paddingBottom: "1em" }}>Type</span>
+				<hr
+					style={{
+						width: "100%",
+						border: "none",
+						borderTop: "1px solid #E5E5E5",
+					}}
+				/>
 				<div className="pokemon-card-stats">
-					<div className="pokemon-stats">
-						<p>HP</p> 
-						<span>{pokemon.stats && pokemon.stats[0].base_stat}</span>
-					</div>
-					<div className="pokemon-stats">
-						<p>ATTACK</p> 
-						<span>{pokemon.stats && pokemon.stats[1].base_stat}</span>
-					</div>
-					<div className="pokemon-stats">
-						<p>DEFENSE</p> <span>{pokemon.stats && pokemon.stats[2].base_stat}</span>
-					</div>
-					<div className="pokemon-stats">
-						<p>SPEED</p> <span>{pokemon.stats && pokemon.stats[5].base_stat}</span>
-					</div>
-				</div> 
+					{pokemon?.stats?.map(stat => {
+						if (
+							pokemon.stats?.indexOf(stat) === 3 ||
+							pokemon.stats?.indexOf(stat) === 4
+						)
+							return;
+						return (
+							<div key={stat.stat.name} className="pokemon-stats">
+								<p>
+									{stat.stat.name.charAt(0).toUpperCase() +
+										stat.stat.name.slice(1)}
+								</p>
+								<span style={{ color: textColor }}>{stat.base_stat}</span>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
