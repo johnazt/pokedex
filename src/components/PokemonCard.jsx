3477f541300a25fcb6725c20a3d7093d060995ca
johnazt <john.azt.dev@gmail.com>
2022-10-30 22:54:10 -0300

    Fix pokemon-card-img

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -183,7 +183,10 @@ const PokemonCard = props => {
 					</div>
 				</div>
 			) : (
+					<div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh"}}>
+
 				<CircularProgress />
+					</div>
 			)}
 		</>
 	);
